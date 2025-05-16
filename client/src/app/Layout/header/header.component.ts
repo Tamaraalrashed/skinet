330fcce06a384df3873fc95443aca76b49a74be6
tamara_almanaseer <tamara_almanaseer@live.com>
2025-05-16 16:10:40 +0300

    Angular Basics Add

---
+++ client/src/app/Layout/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {MatButton} from '@angular/material/button';
+import {MatBadge} from '@angular/material/badge';
+import {MatIcon} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    MatBadge,
+MatIcon,
+    MatButton
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './Layout/header/header.component';
+import {HttpClient} from '@angular/common/http';
+import {Pagination} from './Shared/Models/pagination';
+import {Product} from './Shared/Models/product';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  title = 'skinet';
+  baseUrl='https://localhost:5001/api/';
+  private http=inject(HttpClient);
+  products:Product[] = [];
+  ngOnInit(){
+    this.http.get<Pagination<Product>>(this.baseUrl+'products').subscribe({
+      next: response => this.products =response.data,
+      error: err => {console.log(err)}
+      }
+    )
+   }
 }
